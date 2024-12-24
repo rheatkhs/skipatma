ba47a290d221cf283e44b43f39c4188f8b1c960c
Rhea Takahashi <rheatkhs@gmail.com>
2024-12-24 15:24:57 +0700

    feat(admin): add sign in and sign out routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,5 @@ $routes->post('/sendMessage', 'Siswa::sendMessage');
 $routes->post('/storePendaftaran', 'Siswa::storePendaftaran');
 $routes->get('/admin/dashboard', 'Admin::index');
 $routes->get('/admin/data_siswa', 'Admin::data_siswa');
+$routes->get('/admin/sign_in', 'Admin::sign_in');
+$routes->get('/admin/sign_out', 'Admin::sign_out');
